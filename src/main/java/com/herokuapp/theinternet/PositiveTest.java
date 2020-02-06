86127c8556fe2ad682e7326545712cdf649142f2
sabathrodriguez <sabathcasillas1@gmail.com>
2020-02-06 12:30:43 -0700

    we added goals and chrome driver.

---
+++ src/main/java/com/herokuapp/theinternet/PositiveTest.java
@@ -0,0 +1,30 @@
+package com.herokuapp.theinternet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PositiveTest {
+
+    public void loginTest() {
+//        create driver
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+//        open test page
+
+//        enter username
+
+//        enter password
+
+//        click login button
+
+//        verification:
+
+//        new url:
+
+//        logout button is visible
+
+//        successful login message
+
+
+    }
+}
