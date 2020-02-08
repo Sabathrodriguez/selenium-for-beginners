4e9436d48b4d6d57e097dfa7a6ac649361738252
sabathrodriguez <sabathcasillas1@gmail.com>
2020-02-08 01:21:50 -0700

    we ran our first webdriver test using the console.

--- src/test/java/com/herokuapp/theinternet/PositiveTest.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTest.java
@@ -2,14 +2,25 @@ package com.herokuapp.theinternet;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class PositiveTest {
 
+    @Test
     public void loginTest() {
 //        create driver
+        System.out.println("Starting loginTest");
+
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
+
 //        open test page
+        String url = "http://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
+
+        //maximize driver window
+        driver.manage().window().maximize();
 
 //        enter username
 
@@ -25,6 +36,8 @@ public class PositiveTest {
 
 //        successful login message
 
+        //close brower
+        driver.quit();
 
     }
 }
