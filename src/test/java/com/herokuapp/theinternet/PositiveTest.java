4fafa6e01a89a3f1ac8d2f2a663c3817b5605bd4
sabathrodriguez <sabathcasillas1@gmail.com>
2020-02-09 19:34:34 -0700

    we wrote tests for: username, password, login button, and logout button.

--- src/test/java/com/herokuapp/theinternet/PositiveTest.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTest.java
@@ -1,6 +1,8 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -23,19 +25,21 @@ public class PositiveTest {
         driver.manage().window().maximize();
 
 //        enter username
-
+        WebElement username = driver.findElement(By.id("username"));
 //        enter password
-
+        WebElement password = driver.findElement(By.xpath("/html//input[@id='password']"));
 //        click login button
+        WebElement loginButton = driver.findElement(By.xpath("//form[@id='login']/button[@class='radius']"));
 
 //        verification:
 
 //        new url:
 
 //        logout button is visible
+        WebElement logoutButton = driver.findElement(By.xpath("div[@id='content']//a[@href='/logout']"));
 
 //        successful login message
-
+        WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
         //close brower
         driver.quit();
 
